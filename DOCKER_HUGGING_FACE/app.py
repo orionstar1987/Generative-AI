336a2406481db8d5ccd23dddeb6bd9de56a1eb27
orionstar1987 <haoyongc@gmail.com>
2024-06-20 22:04:28 -0400

    commit

---
+++ DOCKER_HUGGING_FACE/app.py
@@ -0,0 +1,23 @@
+from fastapi import FASTAPI
+from transformers import pipeline
+
+## Create a new FASTAPI app instance
+app = FASTAPI()
+
+## Initialize the text generation pipeline
+pipe = pipeline("text2text-generation", model="google/flan-t5-small")
+
+@app.get("/")
+def home():
+    return {"message": "Hello World"}
+
+
+## Define a function to handle the GET request at '/generate'
+
+@app.get("/generate")
+def generate(text: str):
+    ## use the pipeline to generate txt from given input text
+    output = pipe(text)
+
+    ## return the generate text in JSON response
+    return {"output": output[0]['generated_text']}    
